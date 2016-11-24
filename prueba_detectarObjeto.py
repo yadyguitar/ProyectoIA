bbc5dec7e56e000dbd9db131e319aea22f19b10a
Yadira Acer <yadyguitar@yahoo.es>
2016-11-23 20:55:53 -0600

    skdghds

--- prueba_detectarObjeto.py
+++ prueba_detectarObjeto.py
@@ -1,14 +1,13 @@
 import cv2
 import cv
-import sys
-import numpy as np
+
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 
-out = cv2.VideoWriter('output.avi',cv.CV_FOURCC('D','I','V','X'), 25, (640,480))
+out=cv2.VideoWriter('output.avi',cv.CV_FOURCC('D','I','V','X'), 25, (640,480))
 
 
 
@@ -23,6 +22,7 @@ while (True):
 
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	print faces
 	for (x,y,w,h) in faces:
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 		roi_gray = gray[y:y+h, x:x+w]
